7ebf64989602ac886136c84f1ed29a3969db67a9
Nicholas Landau <nrlandau@gmail.com>
2018-10-11 13:04:36 -0400

    created isVowel Method

--- Program.cs
+++ Program.cs
@@ -4,9 +4,25 @@ namespace Capstone1
 {
     class Program
     {
+        static string WordToPigLatin(string inWord)
+        {
+
+            return "";
+        }
+        static bool IsVowel(char letter)
+        {
+            switch(letter)
+            {
+                case 'a': case 'e': case 'i': case 'o': case 'u':
+                case 'A': case 'E': case 'I': case 'O': case 'U':
+                return true;
+                default:
+                return false;
+            }
+        }
         static void Main(string[] args)
         {
-            
+            ;
         }
     }
 }
